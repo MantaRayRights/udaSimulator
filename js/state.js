6ae9a3902dc8e29cc8f082319169182de8bec5aa
MantaRayRights <870212900@qq.com>
2026-06-03 15:29:58 +0900

    added the dead state

--- js/state.js
+++ js/state.js
@@ -1,6 +1,6 @@
 export const state = {
   money: 0, hp:100, sleep: 100, mood: 100,
-  active: {'idle': true, 'working': false, 'eating':false, 'sleeping': false, 'drinking': false},
+  active: {'idle': true, 'working': false, 'eating':false, 'sleeping': false, 'drinking': false, "dead": false},
   passive: { sleepy: false, sad: false },
 };
 
