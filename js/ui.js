26871f2cc48ab92b3eaeafe77e782030d2963ae3
MantaRayRights <870212900@qq.com>
2026-06-03 15:46:49 +0900

    fuck the bars

--- js/ui.js
+++ js/ui.js
@@ -4,7 +4,7 @@ export function updateBars(state) {
   document.getElementById('bar-hp').style.width    = state.hp     + '%';
   document.getElementById('bar-sleep').style.width = state.sleep  + '%';
   document.getElementById('bar-mood').style.width= state.mood + '%';
-  document.getElementById('bar-money').style.width = (state.money / MAX_MONEY * 100) + '%';
+  document.getElementById('bar-money').style.width = (state.money / 999 * 100) + '%';
 }
 
 export function updateBarColors(state) {
